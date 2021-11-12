4c6297e4c4e6ab6228e19f6896e9aee2b36f9fe1
DataChefBoyardee <74621580+DataChefBoyardee@users.noreply.github.com>
2021-11-11 20:44:17 -0500

    Added Skyrim memes to the main window

--- GUI/main.py
+++ GUI/main.py
@@ -1,9 +1,15 @@
+#-----------------------------------------------------------------------------------------------------------
+#
+#                                           BOOTLEG STEAM
+#
+#-----------------------------------------------------------------------------------------------------------
 import sys 
 from mainLogin import Ui_Login_Dialog
 from createAcc import Ui_Sign_Up_Dialog
+from mainWindow import Ui_MainWindow
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtCore as qtc
-from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow
 
 class Login(QDialog, Ui_Login_Dialog):
     def __init__(self):
@@ -11,6 +17,8 @@ class Login(QDialog, Ui_Login_Dialog):
         self.setupUi(self)
 
         self.loginButton.clicked.connect(self.authenticate)
+        self.loginButton.clicked.connect(self.goToMainWindow)
+        self.loginButton.clicked.connect(self.expand_dong)
         self.signUpButton.clicked.connect(self.goToCreateAcc)
 
     def authenticate(self):
@@ -23,9 +31,19 @@ class Login(QDialog, Ui_Login_Dialog):
         else:
             qtw.QMessageBox.critical(self, 'Fail', 'You did not log in.')
 
+    def expand_dong(self):
+        self.resize(952, 700)
+        self.setMaximumHeight(952)
+        self.setMaximumWidth(700)
+
     def goToCreateAcc(self):
         create = CreateAcc()
         widget.addWidget(create)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+    def goToMainWindow(self):
+        window = Main_Window()
+        widget.addWidget(window)
         widget.setCurrentIndex(widget.currentIndex()+1)
 
 class CreateAcc(QDialog, Ui_Sign_Up_Dialog):
@@ -46,6 +64,15 @@ class CreateAcc(QDialog, Ui_Sign_Up_Dialog):
         else:
             qtw.QMessageBox.critical(self, 'Fail', 'You\'re passwords didn\'t match, please reenter your password.')
 
+class Main_Window(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(Main_Window, self).__init__()
+        self.setupUi(self)
+    
+        self.pushButton.clicked.connect(self.borderCrossing)
+
+    def borderCrossing(self):
+        qtw.QMessageBox.critical(self, '*Skyrim Noises*', 'You were trying to cross the border, just like that horse thief over there.')
 
 app = QApplication(sys.argv)
 mainWindow = Login()
